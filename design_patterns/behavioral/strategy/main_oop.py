005aa5c82c7c44e0cea5e1753b48ba3e98c15aff
daniyal1998 <daniyalhammad1998@gmail.com>
2024-09-30 11:06:05 +0530

    added classic oop strategy pattern

---
+++ design_patterns/behavioral/strategy/main_oop.py
@@ -0,0 +1,25 @@
+import typing
+from strategy_oop import OrderingStrategy, ItemProcessing, FILOStrategy
+
+
+class BlackHoleStrategy(OrderingStrategy):
+    def order_items(self, items: typing.Sequence[typing.Any]) -> typing.List:
+        return []
+
+
+def main():
+    # create the application
+    app = ItemProcessing()
+
+    # create a few items
+    app.add_item("Air Pods")
+    app.add_item("Paint Brush")
+    app.add_item("Gloves")
+
+    # process items
+    app.process_items(BlackHoleStrategy())
+    app.process_items(FILOStrategy())
+
+
+if __name__ == "__main__":
+    main()
---
+++ design_patterns/behavioral/strategy/strategy_oop.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+import typing
+import random
+
+
+class OrderingStrategy(ABC):
+    @abstractmethod
+    def order_items(self, items: typing.Sequence[typing.Any]) -> typing.List:
+        """returns an ordered list of items"""
+
+
+class FIFOStrategy(OrderingStrategy):
+    """First in First out ordering of items"""
+
+    def order_items(self, items: typing.Sequence[typing.Any]) -> typing.List:
+        return [item for item in items]
+
+
+class FILOStrategy(OrderingStrategy):
+    """First in Last out ordering of items"""
+
+    def order_items(self, items: typing.Sequence[typing.Any]) -> typing.List:
+        return list(reversed(items))
+
+
+class RandomStrategy(OrderingStrategy):
+    """Random ordering of items"""
+
+    def order_items(self, items: typing.Sequence[typing.Any]) -> typing.List:
+        return random.sample(items, len(items))
+
+
+class ItemProcessing:
+    def __init__(self) -> None:
+        self.items: list = []
+
+    def add_item(self, item: typing.Any) -> None:
+        self.items.append(item)
+
+    def process_items(self, processing_strategy: OrderingStrategy):
+        # create the ordered list
+        item_list = processing_strategy.order_items(self.items)
+
+        # if it's empty, don't do anything
+        if not item_list:
+            print("There are no items to process!!")
+            return
+
+        # go through the items in the list
+        for item in item_list:
+            print(item, end=" --> ")
+        print("None")
